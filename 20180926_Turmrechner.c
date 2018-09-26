204ad831d5546d44e9fefea371e5c3812e87b4a7
Kai <43405998+PuzZzeLz@users.noreply.github.com>
2018-09-26 08:28:12 +0200

    Create 20180926_Turmrechner.c

---
+++ 20180926_Turmrechner.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main()
+{
+    int start = 0;
+    int result = 0;
+    
+    printf("Geben Sie einen Startwert ein: ");
+    scanf("%d",&start);
+    result = start;
+    printf("Wir rechnen einen Turm mit dem Startwert %d\n", start);
+    
+    for(unsigned int i=1; i<=10; i++){
+        int tmpResult = result * i;
+        printf("%d * %d = %d\n", result, i, tmpResult);
+        result = tmpResult;
+    }
+    
+    for(unsigned int i=1; i<=10; i++){
+        int tmpResult = result / i;
+        printf("%d / %d = %d\n", result, i, tmpResult);
+        result = tmpResult;
+    }
+    if(result == start) {
+        printf("Juhu es passt\n");
+    } else {
+        printf("Geh Fehler suchen!\n");
+    }
+}
