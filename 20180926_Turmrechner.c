89eb2fcc48bb48b45cab15519862a74eef463d6c
Kai <43405998+PuzZzeLz@users.noreply.github.com>
2018-09-26 08:40:00 +0200

    Update 20180926_Turmrechner.c

--- 20180926_Turmrechner.c
+++ 20180926_Turmrechner.c
@@ -1,30 +1,36 @@
 #include <stdio.h>
 
-int main()
+int
+main ()
 {
-    int start = 0;
-    int result = 0;
-    
-    printf("Geben Sie einen Startwert ein: ");
-    scanf("%d",&start);
-    result = start;
-    printf("Wir rechnen einen Turm mit dem Startwert %d\n", start);
-    
-    for(unsigned int i=1; i<=10; i++){
-        int tmpResult = result * i;
-        printf("%d * %d = %d\n", result, i, tmpResult);
-        result = tmpResult;
+  int start = 0;
+  int result = 0;
+
+  printf ("Geben Sie einen Startwert ein: ");
+  scanf ("%d", &start);
+  result = start;
+  printf ("Wir rechnen einen Turm mit dem Startwert %d\n", start);
+
+  for (unsigned int i = 1; i <= 10; i++)
+    {
+      int tmpResult = result * i;
+      printf ("%d * %d = %d\n", result, i, tmpResult);
+      result = tmpResult;
     }
-    
-    for(unsigned int i=1; i<=10; i++){
-        int tmpResult = result / i;
-        printf("%d / %d = %d\n", result, i, tmpResult);
-        result = tmpResult;
+
+  for (unsigned int i = 1; i <= 10; i++)
+    {
+      int tmpResult = result / i;
+      printf ("%d / %d = %d\n", result, i, tmpResult);
+      result = tmpResult;
     }
-    
-    if(result == start) {
-        printf("Juhu es passt\n");
-    } else {
-        printf("Geh Fehler suchen!\n");
+
+  if (result == start)
+    {
+      printf ("Juhu es passt!\n");
+    }
+  else
+    {
+      printf ("Geh Fehler suchen!\n");
     }
 }
