f10b3c4de2eb69b622a161f1ca9805a713d7fc00
Kai <43405998+PuzZzeLz@users.noreply.github.com>
2018-11-07 08:38:14 +0100

    Update 20181107_Uebung_Rec.c

--- 20181107_Uebung_Rec.c
+++ 20181107_Uebung_Rec.c
@@ -1,8 +1,22 @@
 #include <stdio.h>
 
+int summe(int value) {
+	int sum = 0;
+	for(int i = 0; i <= value; value--) {
+		sum = sum + value;
+	} return sum;
+}
+
+int summeRec(int value, int sum) {
+
+    if(value) {
+        sum = value + summeRec(value -1, sum);
+    } return sum;
+}
+
 int sumNumbers(int numbers[], int length){
 	int sum = 0;
-	for(int i = 0; i < length; i++){
+	for(int i = 0; i <= length; i++){
 		sum = sum + numbers[i];
 	}
 	return sum;
@@ -17,7 +31,10 @@ int sumNumbersRec(int numbers[], int length )
 
 int main(){
 	int myArray[] = {1,2,3,4,5};
+	int value = 5;
 	
-	printf("sumNumbers: %d\n", sumNumbersRec(myArray, 4));
-	printf("sumNumbersRec: %d\n", sumNumbersRec(myArray, 4));
+	printf("Summe: %d\n", summe(value));
+	printf("Summe Rec: %d\n", summeRec(value,0));
+	printf("Array: sumNumbers: %d\n", sumNumbers(myArray, 4));
+	printf("Array: sumNumbersRec: %d\n", sumNumbersRec(myArray, 4));
 }
