700cb24dce5122ec96cf7b1cb70f5714690a6a6e
Kai <43405998+PuzZzeLz@users.noreply.github.com>
2018-11-21 08:52:27 +0100

    Update Zahlensystem.c

--- Schuluebungen/Zahlensystem.c
+++ Schuluebungen/Zahlensystem.c
@@ -1,31 +1,34 @@
 #include <stdio.h>
 #include <math.h>
 
+int toTen(int value, int basis) {
+    int temp = value % 10;
+	int digit1 = temp;
+
+	value = value - temp;
+	temp = value % 100;
+	int digit2 = temp / 10;
+
+	value = value - temp;
+	temp = value % 1000;
+	int digit3 = temp / 100;
+	
+	int result = digit3 * pow(basis, 2) + digit2 * pow(basis, 1) + digit1 * pow(basis, 0);
+	return result;
+}
+
+int fromTen(int value, int basis) {
+    return 0;
+}
+
 int main() {
 	int value = 0;
 	int basis = 0;
-	int temp = 0;
-	int digit1 = 0;
-	int digit2 = 0;
-	int digit3 = 0;
-	int result = 0;
 	
 	printf("Startwert: \n");
 	scanf("%d",&value);
 	printf("Basis: \n");
 	scanf("%d",&basis);
 	
-	temp = value % 10;
-	digit1 = temp;
-
-	value = value - temp;
-	temp = value % 100;
-	digit2 = temp / 10;
-
-	value = value - temp;
-	temp = value % 1000;
-	digit3 = temp / 100;
-	
-	result = digit3 * pow(basis, 2) + digit2 * pow(basis, 1) + digit1 * pow(basis, 0);
-	printf("Result: %d\n", result);
+	printf("Result: %d\n", toTen(value, basis));
 }
