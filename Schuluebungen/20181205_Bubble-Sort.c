516c1aa38cc2fd568191b26b3c70cd5b50346a09
Kai <43405998+PuzZzeLz@users.noreply.github.com>
2018-12-05 09:27:33 +0100

    Create 20181205_Bubble-Sort.c

---
+++ Schuluebungen/20181205_Bubble-Sort.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <string.h>
+
+void createArray(int *Array, int length) {
+    //srand(time(0); for random
+    int rng = rand();
+    Array[length] = rng;
+}
+
+void showArrayInConsole(int *Array, length) {
+    printf("Array: %s\nLength: %s\n", Array, length)
+}
+
+void switchNumbers(int *Array, int position) {
+    
+}
+
+
+
+int main()
+{
+    int numbers[];
+    int length = 10;
+    
+    createArray(numbers, length);
+    showArrayInConsole(numbers, length);
+    
+    return 0;
+}
