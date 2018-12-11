419fe30e686a7e141aa9a33b64dbc6b7ebe3bd2c
Kai <43405998+PuzZzeLz@users.noreply.github.com>
2018-12-11 14:42:37 +0100

    Update 20181205_Bubble-Sort.c

--- Schuluebungen/20181205_Bubble-Sort.c
+++ Schuluebungen/20181205_Bubble-Sort.c
@@ -1,28 +1,39 @@
 #include <stdio.h>
 #include <string.h>
 
-void createArray(int *Array, int length) {
-    //srand(time(0)); for random
-    int rng = rand();
-    Array[length] = rng;
+void createArray(int Array[], int length) {
+    srand(time(0));
+    for(int i = 0; i < length; i++) {
+        int rng = rand();
+        Array[i] = rng%100;
+    }
 }
 
-void showArrayInConsole(int *Array, length) {
-    printf("Array: %s\nLength: %s\n", Array, length)
+void showArrayInConsole(int *Array, int length) {
+    for(int i = 0; i < length; i++) {
+        printf("%d ", Array[i]);
+    }
 }
 
-void switchNumbers(int *Array, int position) {
-    
+void bubbleSort(int *Array, int position) {
+    for() {
+        
+    }
 }
 
 
 
 int main()
 {
-    int numbers[];
     int length = 10;
+    int numbers[length];
+
     
     createArray(numbers, length);
+    printf("Array mit %d zufaelligen Zahlen:\n", length);
+    showArrayInConsole(numbers, length);
+    bubbleSort(numbers, length);
+    printf("\nArray mit %d zufaelligen Zahlen sortiert:\n", length);
     showArrayInConsole(numbers, length);
     
     return 0;
