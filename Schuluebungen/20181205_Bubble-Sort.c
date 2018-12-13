8feebc880f43737cef7fbb8421c9326aa7965848
Kai <43405998+PuzZzeLz@users.noreply.github.com>
2018-12-13 12:20:49 +0100

    Update 20181205_Bubble-Sort.c

--- Schuluebungen/20181205_Bubble-Sort.c
+++ Schuluebungen/20181205_Bubble-Sort.c
@@ -72,7 +72,7 @@ int main()
     showArrayInConsole(numbers, length);
     
     printf("\n\nMaximalzahl der nötigen Vergleiche wäre (%d-1)*(%d-1) = %d\n", length, length, (length-1)*(length-1));
-    printf("Verbesserte Version (pro Durchlauf 1 Vergleich weniger) wäre (%d + %d + %d + ... + 3 + 2 + 1) = %d\n", length-1, length-2, length-3, gaus);
+    printf("Verbesserte Version (pro Durchlauf 1 Vergleich weniger) wäre (%d + %d + %d + ... + 3 + 2 + 1) = %d\n", length, length-1, length-2, gaus);
     printf("Tatsächlich benötigte Vergleiche (Ausstieg mit break, wenn fertig sortiert): %d\n", numCompare);
     
     return 0;
