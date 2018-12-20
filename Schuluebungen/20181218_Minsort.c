ea3f88e0a69bc1b7ff4be403d373277286e97d83
Kai <43405998+PuzZzeLz@users.noreply.github.com>
2018-12-20 12:04:42 +0100

    Update 20181218_Minsort.c

--- Schuluebungen/20181218_Minsort.c
+++ Schuluebungen/20181218_Minsort.c
@@ -1,6 +1,59 @@
 #include <stdio.h>
 
+void createArray(int *array, int length)
+{
+    srand(time(0));
+    
+    for(int i = 0; i < length; i++)
+    {
+        int rng = rand();
+        array[i] = rng%100;
+    }
+}
+
+void showArrayInConsole(int *array, int length)
+{
+    for(int i = 0; i < length; i++)
+    {
+        printf("%d ", array[i]);
+    }
+}
+
+int arrayMinIndex(int *x, int start, int size) {
+    int minIndex = start, i;
+    for(i = start; i < size; i++) {
+        if(x[i] < x[minIndex]) {
+            minIndex = i;
+        }
+    }
+    return minIndex;
+}
+
+void arrayMinSort(int *x, int size) {
+    int i, temp, minIndex;
+    for(i = 0; i < size-1; i++) {
+        minIndex = arrayMinIndex(x, i, size);
+        if(i != minIndex) {
+        temp = x[i];
+        x[i] = x[minIndex];
+        x[minIndex] = temp;
+        }
+    }
+}
+
 int main()
 {
+    int length = 10;
+    int numbers[length];
+    
+    createArray(numbers, length);
+    printf("Array mit %d zufaelligen Zahlen:\n", length);
+    showArrayInConsole(numbers, length);
+    
+    arrayMinSort(numbers, length);
+    
+    printf("\nArray mit %d zufaelligen Zahlen sortiert:\n", length);
+    showArrayInConsole(numbers, length);
+    
     return 0;
 }
