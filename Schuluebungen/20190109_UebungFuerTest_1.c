813a08f3f35870c2d566a20dee0cccb443b2a94f
Kai <43405998+PuzZzeLz@users.noreply.github.com>
2019-01-15 14:53:53 +0100

    Update 20190109_UebungFuerTest_1.c

--- Schuluebungen/20190109_UebungFuerTest_1.c
+++ Schuluebungen/20190109_UebungFuerTest_1.c
@@ -46,6 +46,15 @@ void differFromAvg(int * array, int length, int average) {
     }
 }
 
+/*int maxArray(int * array, int length) {
+    int max = array[0];
+    for (int i = 0; i < length; i++) {
+       if (array[i] > max) {
+           max = array[i];
+       }
+   } return max;
+}*/
+
 int main() {
     int length = 10;
     int numbers[length];
